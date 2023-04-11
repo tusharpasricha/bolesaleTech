f579dc325ec5d1ecb7ba13ec2efae192a84a09e6
tusharpasricha <tusharpasricha21@gmail.com>
2023-04-11 11:12:02 +0530

    css

--- src/components/getintouch.js
+++ src/components/getintouch.js
@@ -34,20 +34,18 @@ const Getintouch = () => {
     return (
         <>
         <form className='form' onSubmit={submitHandler}>
-        <h2>Get in Touch</h2>
-      <label htmlFor="name">Name</label>
-            <input type="text" value={name} onChange={e => setName(e.target.value)} />
-            <label htmlFor="email">Email</label>
-            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-            <label htmlFor="contactNumber">Contact Number</label>
+        <h1>Get in Touch</h1>
+      
+            <input placeholder='Name' type="text" value={name} onChange={e => setName(e.target.value)} />
+            
+            <input placeholder='Email' type="text" value={email} onChange={e => setEmail(e.target.value)} />
+          
 
-            <input type="text" value={contactNumber} onChange={e => setContactNumber(e.target.value)} />
-            <label htmlFor="purpose">Purpose</label>
-
-            <input type="text" value={purpose} onChange={e => setPurpose(e.target.value)} />
-            <label htmlFor="message">Message</label>
-
-            <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
+            <input placeholder='Contect' type="text" value={contactNumber} onChange={e => setContactNumber(e.target.value)} />
+            
+            <input placeholder='Purpose' type="text" value={purpose} onChange={e => setPurpose(e.target.value)} />
+            
+            <input placeholder='Message' type="text" value={message} onChange={e => setMessage(e.target.value)} />
             <button type='submit'>Add</button>
         </form>
         
